496f55ae46e8bb2f16151d78275926d1e5d699f7
RedZine <juan.atuesta.y@mail.pucv.cl>
2025-10-31 21:33:45 -0300

    Creación de clases, abstractas e interfaces.

---
+++ core/src/main/java/io/github/some_example_name/DropBlue.java
@@ -0,0 +1,5 @@
+package io.github.some_example_name;
+
+public class DropBlue extends Collectible {
+
+}
---
+++ core/src/main/java/io/github/some_example_name/PowerCleanScreen.java
@@ -0,0 +1,5 @@
+package io.github.some_example_name;
+
+public class PowerCleanScreen implements Power {
+
+}
